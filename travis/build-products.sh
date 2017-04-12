6091236b523e74dda0b6c624ef6cb2e70c7a252f
Philippe Merle <Philippe.merle@inria.fr>
2017-04-12 15:45:41 +0200

    Add scripts

---
+++ travis/build-products.sh
@@ -0,0 +1,17 @@
+
+# Are we on the right branch?
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  
+  # Is this not a Pull Request?
+  if [ "$TRAVIS_PULL_REQUEST" = false ]; then
+    
+    # Is this not a build which was triggered by setting a new tag?
+    if [ -z "$TRAVIS_TAG" ]; then
+
+      cd clouddesigner/org.occiware.clouddesigner.parent
+      mvn clean verify -Pbuild.products
+
+   fi
+  fi
+fi
+
